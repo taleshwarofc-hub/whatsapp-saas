a80961a3947ee981e98536c6044356316cd8b34c
taleshwarofc-hub <taleshwarofc@gmail.com>
2025-11-24 18:18:18 +0530

    Create server.js

---
+++ backend/backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("WhatsApp SaaS Backend Running...");
+});
+
+app.listen(5000, () => console.log("Server live on port 5000"));
